c868630baea427fec37c88ac6442418dc37214c0
tambarie <gt.gbaragbo@gmail.com>
2022-03-10 14:36:07 +0100

    add change user status method

--- application/walletRepo/walletRepository.go
+++ application/walletRepo/walletRepository.go
@@ -5,6 +5,7 @@ import (
 	"github.com/Tambarie/wallet-engine/domain/wallet"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"golang.org/x/net/context"
 	"log"
 )
@@ -62,14 +63,14 @@ func (walletRepo *RepositoryDB) SaveTransaction(t *wallet.Transaction) (*wallet.
 	return t, err
 }
 
-func (walletRepo *RepositoryDB) PostToAccount(a *wallet.Account) (*wallet.Account, error) {
+func (walletRepo *RepositoryDB) PostToAccount(a *wallet.Wallet) (*wallet.Wallet, error) {
 	coll := walletRepo.db.Database("opay").Collection("account-balance")
 	result, err := coll.InsertOne(context.TODO(), a)
 	fmt.Printf("Inserted document with _id: %v\n", result.InsertedID)
 	return a, err
 }
 
-func (walletRepo *RepositoryDB) GetAccountBalance(userID string) ([]*wallet.Account, error) {
+func (walletRepo *RepositoryDB) GetAccountBalance(userID string) ([]*wallet.Wallet, error) {
 	coll := walletRepo.db.Database("opay").Collection("account-balance")
 
 	filter := bson.D{{"userid", userID}}
@@ -77,10 +78,33 @@ func (walletRepo *RepositoryDB) GetAccountBalance(userID string) ([]*wallet.Acco
 	if err != nil {
 		panic(err)
 	}
-	var results []*wallet.Account
+	var results []*wallet.Wallet
 	if err = cursor.All(context.TODO(), &results); err != nil {
 		log.Fatal(err)
 	}
 	return results, err
 
+}
+
+func (walletRepo *RepositoryDB) ChangeUserStatus(isActive bool, userReference string) (interface{}, error) {
+	coll := walletRepo.db.Database("opay").Collection("opay-collection")
+
+	filter := bson.D{{"reference", userReference}}
+	opts := options.Update().SetUpsert(true)
+	update := bson.D{{"$set", bson.D{{"is_active", isActive}}}}
+
+	result, err := coll.UpdateOne(context.TODO(), filter, update, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if result.MatchedCount != 0 {
+		fmt.Println("matched and replaced an existing document")
+
+	}
+	if result.UpsertedCount != 0 {
+		fmt.Printf("inserted a new document with ID %v\n", result.UpsertedID)
+	}
+
+	return result.UpsertedID, err
 }
