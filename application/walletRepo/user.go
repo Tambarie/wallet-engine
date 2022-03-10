9126858f1c17ee0c3f6c491c6e1e68e18daff0d1
tambarie <gt.gbaragbo@gmail.com>
2022-03-10 14:33:19 +0100

    add change user status signature to the repository interface

--- application/walletRepo/user.go
+++ application/walletRepo/user.go
@@ -6,7 +6,8 @@ type Repository interface {
 	CreateWallet(u *wallet.User) (*wallet.User, error)
 	GetUserByEmail(email string) ([]*wallet.User, error)
 	CheckIfPasswordExists(userReference string) ([]*wallet.User, error)
-	PostToAccount(u *wallet.Account) (*wallet.Account, error)
+	PostToAccount(u *wallet.Wallet) (*wallet.Wallet, error)
 	SaveTransaction(t *wallet.Transaction) (*wallet.Transaction, error)
-	GetAccountBalance(userID string) ([]*wallet.Account, error)
+	GetAccountBalance(userID string) ([]*wallet.Wallet, error)
+	ChangeUserStatus(isActive bool, userReference string) (interface{}, error)
 }
