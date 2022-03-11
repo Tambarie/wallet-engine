ab65cbb6ac0973f5f0b78e9ed22a4539a0f0f1fe
tambarie <gt.gbaragbo@gmail.com>
2022-03-11 09:50:05 +0100

    comments added

--- domain/wallet/wallet.go
+++ domain/wallet/wallet.go
@@ -1,5 +1,6 @@
 package wallet
 
+// Transaction struct
 type Transaction struct {
 	UserID               string
 	TransactionReference string
@@ -8,17 +9,20 @@ type Transaction struct {
 	Password             string
 }
 
+// Wallet struct
 type Wallet struct {
 	UserID    string
 	Balance   float64
 	Reference string
 }
 
+// CreditUserWallet Credits the user data
 func (w *Wallet) CreditUserWallet(money float64, userID string) {
 	w.UserID = userID
 	w.Balance += money
 }
 
+// DebitUserWallet Debits the user data
 func (w *Wallet) DebitUserWallet(money float64, userID string) {
 	w.UserID = userID
 	w.Balance -= money
