e3c6a2072adec7627d9fbfe5a8154aadf44826fe
tambarie <gt.gbaragbo@gmail.com>
2022-03-11 09:48:52 +0100

    comments added

--- application/handler/createUser.go
+++ application/handler/createUser.go
@@ -12,8 +12,10 @@ import (
 	"time"
 )
 
-func (h *Handler) CreateUser() gin.HandlerFunc {
+// CreateWallet Handler to create wallet for user
+func (h *Handler) CreateWallet() gin.HandlerFunc {
 	return func(context *gin.Context) {
+
 		var user = &wallet.User{}
 		hashedPassword, err := helpers.GenerateHashPassword(user.Password)
 		if err != nil {
@@ -23,18 +25,22 @@ func (h *Handler) CreateUser() gin.HandlerFunc {
 		user.Reference = uuid.New().String()
 		user.CreatedAt = time.Now().UTC()
 		user.HashedSecretKey = string(hashedPassword)
-		//log.Println(user.Password)
+
+		// Binding the json
 		if errs := helpers.Decode(context, &user); errs != nil {
 			fmt.Println(errs)
 			response.JSON(context, http.StatusInternalServerError, nil, errs, "")
 			return
 		}
 
+		// Getting user by email
 		userDB, err := h.WalletService.GetUserByEmail(user.Email)
 		if err != nil {
 			log.Println(err)
 			return
 		}
+
+		// Checking to see if a user already exists
 		if len(userDB) == 0 {
 			userD, err := h.WalletService.CreateWallet(user)
 
@@ -46,7 +52,6 @@ func (h *Handler) CreateUser() gin.HandlerFunc {
 			response.JSON(context, http.StatusCreated, gin.H{"data": userD}, nil, "User created successfully")
 			return
 		} else {
-			log.Println("here")
 			response.JSON(context, http.StatusNotFound, nil, []string{"User email already exists"}, "")
 			return
 		}
